41f194da176c148e39cd0f8aae3692a4623ad656
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-02-07 21:14:30 -0300

    Settings: Port faceunlock feature
    From motorola/nio_retcn/nio:11/RRN31.Q1-20-17-1/1cbc0:user/release-keys
    
    Change-Id: I686b5128405426274b4176d9c09192a6790c82f9

--- src/com/android/settings/security/TopLevelSecurityEntryPreferenceController.java
+++ src/com/android/settings/security/TopLevelSecurityEntryPreferenceController.java
@@ -25,6 +25,8 @@ import com.android.settings.R;
 import com.android.settings.Utils;
 import com.android.settings.core.BasePreferenceController;
 
+import com.android.settings.custom.biometrics.FaceUtils;
+
 public class TopLevelSecurityEntryPreferenceController extends BasePreferenceController {
 
     public TopLevelSecurityEntryPreferenceController(Context context, String preferenceKey) {
@@ -45,7 +47,9 @@ public class TopLevelSecurityEntryPreferenceController extends BasePreferenceCon
         final AppLockManager appLockManager =
                 Utils.getAppLockManager(mContext);
         CharSequence summary = null;
-        if (fpm != null && fpm.isHardwareDetected()) {
+        if (fpm != null && fpm.isHardwareDetected() && FaceUtils.isFaceUnlockSupported()) {
+            summary = mContext.getText(R.string.security_dashboard_summary_face_and_fingerprint);
+        } else if (fpm != null && fpm.isHardwareDetected()) {
             summary = mContext.getText(R.string.security_dashboard_summary);
         } else if (faceManager != null && faceManager.isHardwareDetected()) {
             summary = mContext.getText(R.string.security_dashboard_summary_face);
